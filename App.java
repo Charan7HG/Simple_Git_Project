ed7f4dcd907ad19e0a766ab085b950dec5a44d99
Charan7HG <charanhg1234@gmail.com>
2026-08-19 13:56:40 +0530

    remove github change
    Removed unnecessary output statement to streamline the application.

--- App.java
+++ App.java
@@ -2,7 +2,6 @@
     public static void main(String[] args) {
         System.out.println("Git Mastery Project");
           System.out.println("Learning Git and GitHub");
-        System.out.println("GitHub change");
     }
 }
                                                          
