2cceab7e18122d92e87bdcaf5011a0ef7132eadb
Charan <charan151@gmail.com>
2026-08-19 19:40:34 +0530

    feat: change project message on main

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 ﻿public class App {
     public static void main(String[] args) {
-        System.out.println("Git Mastery Project");
+        System.out.println("Main version");
           System.out.println("Learning Git and GitHub");
           System.out.println("Login feature");
           System.out.println("Login feature implemented successfully");
